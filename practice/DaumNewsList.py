2b11e34a91bb9a399ef714c0d5d4bad014d0ece5
shkabcd <tlsgurud>
2021-10-26 16:54:42 +0900

    Daum News 목록 => 제목, 본문 수집

--- practice/DaumNewsList.py
+++ practice/DaumNewsList.py
@@ -6,12 +6,13 @@ from bs4 import BeautifulSoup
 
 url = 'https://news.daum.net/breakingnews/digital'
 result = requests.get(url)
+
 doc = BeautifulSoup(result.text, 'html.parser')
-url_list = doc.select('ul.list_new2 a.link_txt')
+url_list = doc.select('ul.list_news2 a.link_txt')
 print(len(url_list))
-for url in url_list:
-    print('## NEWS -> {}번###########################################')
-    new_url = url('href')
+for i, url in enumerate(url_list):
+    print('### NEWS -> {}번 #############################################'.format(i+1))
+    new_url = url['href']
     print('# URL: {}'.format(new_url))
     result = requests.get(new_url)  # url의 전체 코드를 가져옴
     doc = BeautifulSoup(result.text, 'html.parser')
@@ -23,7 +24,24 @@ for url in url_list:
     for info in contents:
         content += info.get_text()
 
-
     print('# 뉴스 제목: {}'.format(title))
     print('# 뉴스 본문: {}'.format(content))
 
+
+    # print('## NEWS -> {}번###########################################')
+    # new_url = url['href']
+    # print('# URL: {}'.format(new_url))
+    # result = requests.get(new_url)  # url의 전체 코드를 가져옴
+    # doc = BeautifulSoup(result.text, 'html.parser')
+    # title = doc.select('h3.tit_view')[0].get_text()
+    # contents = doc.select('section p')
+    #
+    # contents.pop(-1)  # 기자 정보 삭제
+    # content = ''  # 본문 총합
+    # for info in contents:
+    #     content += info.get_text()
+    #
+    #
+    # print('# 뉴스 제목: {}'.format(title))
+    # print('# 뉴스 본문: {}'.format(content))
+    #
