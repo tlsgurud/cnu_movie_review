6bc00e782028739cdf8af056c11657e62c484710
shkabcd <tlsgurud>
2021-11-02 16:02:54 +0900

    NAVER MOVIE REVIEW 수집

--- practice/NaverMovieReviewList.py
+++ practice/NaverMovieReviewList.py
@@ -1,6 +1,7 @@
 # NaverMovieReviewList.py
 # -> Naver 영화(1개 선택) 리뷰 정보 수집(review, score, writer, date)
 
+import pprint
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,14 +12,37 @@ doc = BeautifulSoup(result.text, 'html.parser')
 review_list = doc.select('div.score_result > ul > li')
 
 for i, one in enumerate(review_list):
-    print('## REVIEW -> {} #############################################################'.format(i+1))
+    print('## USER -> {} #############################################################'.format(i+1))
+
     # 평점 정보 수집
     score = one.select('div.star_score > em')[0].get_text()
+
     # 리뷰 정보 수집
-    review = one.select('div.score_reple > p > span')
+    review = one.select('div.score_reple > p > span')               #.strip() #텍스트를 기준으로 좌우 공백제거
 
+    if len(review) == 2:
+        review_txt = review[1].get_text().strip()
+    elif len(review) == 1:
+        review_txt = review[0].get_text().strip()
 
+    # j = 0
+    # if len(review) == 2:  # +관람객
+    #     j = 1
+    # review_txt = review[j].get_text().strip()
 
+    # 작성자(닉네임) 정보 수집
+    original_writer = one.select('div.score_reple dt em')[0].get_text().strip()
 
-    print(':: REVIEW: {}'.format((review)))
-    print(':: SCORE: {}'.format(score))
+    idx_end = original_writer.find('(')          #find()# ( 의 인덱스 번호를 줌
+    writer = original_writer[0:idx_end]
+
+    # 날짜 정보 수집
+    original_date = one.select('div.score_reple dt em')[1].get_text()
+
+    # yyyy.mm.dd 전처리 코드 작성
+    date = original_date[:10]
+
+    print(':: REVIEW -> {}'.format(review_txt))
+    print(':: WRITER -> {}'.format(writer))
+    print(':: SCORE -> {}'.format(score))
+    print(':: DATE -> {}'.format(date))
