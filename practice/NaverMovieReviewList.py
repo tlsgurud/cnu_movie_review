38e2b5d1181b303d864f5f40b3418cecfcec1ea7
shkabcd <tlsgurud>
2021-11-04 15:18:39 +0900

    NAVER MOVIE REVIEW 수집(리뷰, 점수, 작성자, 날짜)완료

--- practice/NaverMovieReviewList.py
+++ practice/NaverMovieReviewList.py
@@ -18,17 +18,7 @@ for i, one in enumerate(review_list):
     score = one.select('div.star_score > em')[0].get_text()
 
     # 리뷰 정보 수집
-    review = one.select('div.score_reple > p > span')               #.strip() #텍스트를 기준으로 좌우 공백제거
-
-    if len(review) == 2:
-        review_txt = review[1].get_text().strip()
-    elif len(review) == 1:
-        review_txt = review[0].get_text().strip()
-
-    # j = 0
-    # if len(review) == 2:  # +관람객
-    #     j = 1
-    # review_txt = review[j].get_text().strip()
+    review = one.select('div.score_reple > p > span')[-1].get_text().strip()               #.strip() #텍스트를 기준으로 좌우 공백제거
 
     # 작성자(닉네임) 정보 수집
     original_writer = one.select('div.score_reple dt em')[0].get_text().strip()
@@ -42,7 +32,7 @@ for i, one in enumerate(review_list):
     # yyyy.mm.dd 전처리 코드 작성
     date = original_date[:10]
 
-    print(':: REVIEW -> {}'.format(review_txt))
+    print(':: REVIEW -> {}'.format(review))
     print(':: WRITER -> {}'.format(writer))
     print(':: SCORE -> {}'.format(score))
     print(':: DATE -> {}'.format(date))
